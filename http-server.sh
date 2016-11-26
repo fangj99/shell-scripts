485ba9b42c00c44a55aa7743cfee7d1e134f0c7b
Lsong <song940@gmail.com>
2016-11-26 18:58:33 +0800

    done

--- http-server.sh
+++ http-server.sh
@@ -1,25 +1,51 @@
 #!/usr/bin/env bash
+
 #
-
-response=/tmp/http-server-pipe
+# HTTP WEB SERVER
+# written in pure shell scripts
+# 
+# @song940
+#
+response="/tmp/http-server.pipe"
 mkfifo $response > /dev/null 2>&1
+#
+# parse request
+#
+parse_request(){
+  read request
+  local request_method=$(echo "$request" | cut -d ' ' -f1)
+  local request_path=$(echo "$request" | cut -d ' ' -f2)
+  if [ "$request_path" == "/" ]; then
+    request_path="/index.html"
+  fi
+  # logger
+  echo "$request"
+  # while read request_header; do
+  #   echo "$request_header"
+  # done
+  local filename="$PWD$request_path"
+  if [ -f "$filename" ]; then
+    if [ -x "$filename" ]; then
+      "$filename" > $response 2>&1
+    else
+      cat "$filename" > $response
+    fi
+    return
+  fi
+  echo "Not Found" > $response
+}
 
-while true; do
-  cat $response | nc -l 8080 > >(
-    while read line; do
-      line=$(echo "$line" | tr -d '[\r\n]')
-      # parse http request
-      if echo "$line" | grep -qE '^GET /'; then
-        # request path
-        path=$(echo "$line" | cut -d ' ' -f2)
-        if [ "$path" == "/" ]; then 
-          path="/index" 
-        fi
-        # defaults to html files
-        path="$PWD$path.html"
-        # make http response
-        cat $path > $response 2>&1
-      fi
-    done
-  )
-done
+
+#
+# start_server
+#
+start_server(){
+  local port=$1
+  while true; do
+    cat $response | nc -l $port > \
+    >(parse_request)
+  done
+  rm -f "$response"
+}
+
+start_server 8080

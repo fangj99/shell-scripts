22391fe58716ab24b4ec93e5383e71c64b73a810
Lsøng <song940@gmail.com>
2017-01-12 14:16:13 +0800

    Update progressbar.sh

--- progressbar.sh
+++ progressbar.sh
@@ -3,16 +3,16 @@
 #
 # example:
 # 
-# curl -s https://gist.githubusercontent.com/song940/332b80f24e7f203987eff11fc6765bd8/raw/progressbar.sh | sh
+# curl -s https://raw.githubusercontent.com/song940/shell-scripts/master/progressbar.sh | sh
 #
 
 function ProgressBar(){
   let max=$1
-  let done=$2
-  let left=$max-$done
-  let current=($done*100/$max*100)/100
+  let now=$2
+  let left=$max-$now
+  let current=($now*100/$max*100)/100
 
-  local fill=$(printf "%${done}s")
+  local fill=$(printf "%${now}s")
   local empty=$(printf "%${left}s")
 
   printf "\r [${fill// /#}${empty// /-}] ${current}%%"
